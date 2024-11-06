e16a7042bb693fed30358f8521e10bbbba008838
Cinu13 <marcinturkowskins@gmail.com>
2024-11-06 21:33:36 +0100

    Inicializacja Strony

---
+++ src/test/java/com/example/flicker/FlickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
