db09857308345ddbceaf4243b0cb847c96ef5007
Cinu13 <marcinturkowskins@gmail.com>
2024-11-21 22:55:12 +0100

    css login i register

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import "./Login.css";
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
